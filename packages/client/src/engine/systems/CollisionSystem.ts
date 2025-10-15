c30edd80fcf61b88229250f27eecc595ed7514df
Streltsov Ivan <principdereva@gmail.com>
2025-10-15 21:14:47 +0200

    feat: collision system

--- packages/client/src/engine/systems/CollisionSystem.ts
+++ packages/client/src/engine/systems/CollisionSystem.ts
@@ -1,4 +1,4 @@
-import { ISystem } from '../../types/engine.types'
+import { COMPONENT_TYPES, ISystem } from '../../types/engine.types'
 import {
   PositionComponent,
   CollisionComponent,
@@ -13,18 +13,23 @@ class CollisionSystem implements ISystem {
   private logger = new Logger('CollisionSystem', 'warn')
 
   update(world: World): void {
-    const colliders = world.query('collision', 'position')
+    const colliders = world.query(
+      COMPONENT_TYPES.projectile,
+      COMPONENT_TYPES.collision
+    )
 
     for (let i = 0; i < colliders.length; i++) {
       const a = colliders[i]
-      const posA = a.getComponent<PositionComponent>('position')
-      const colA = a.getComponent<CollisionComponent>('collision')
+      const posA = a.getComponent<PositionComponent>(COMPONENT_TYPES.position)
+      const colA = a.getComponent<CollisionComponent>(COMPONENT_TYPES.collision)
       if (!posA || !colA) continue
 
       for (let j = i + 1; j < colliders.length; j++) {
         const b = colliders[j]
-        const posB = b.getComponent<PositionComponent>('position')
-        const colB = b.getComponent<CollisionComponent>('collision')
+        const posB = b.getComponent<PositionComponent>(COMPONENT_TYPES.position)
+        const colB = b.getComponent<CollisionComponent>(
+          COMPONENT_TYPES.collision
+        )
         if (!posB || !colB) continue
 
         const dx = posA.x - posB.x
@@ -40,22 +45,49 @@ class CollisionSystem implements ISystem {
   }
 
   private handleCollision(a: Entity, b: Entity, world: World) {
-    const aIsProjectile = a.hasComponent('projectile')
-    const bIsProjectile = b.hasComponent('projectile')
+    const aIsProjectile = a.hasComponent(COMPONENT_TYPES.projectile)
+    const bIsProjectile = b.hasComponent(COMPONENT_TYPES.projectile)
 
-    if (aIsProjectile && b.hasComponent('health')) {
-      const projectile = a.getComponent<ProjectileComponent>('projectile')
-      const damage = projectile?.damage ?? 10
-      b.addComponent(new DamageComponent(damage, a.id))
-      this.logger.debug(`Projectile hit enemy`, { id: b.id, damage })
+    if (aIsProjectile) {
+      this.processProjectileHit(a, b, world)
     }
 
-    if (bIsProjectile && a.hasComponent('health')) {
-      const projectile = b.getComponent<ProjectileComponent>('projectile')
-      const damage = projectile?.damage ?? 10
-      a.addComponent(new DamageComponent(damage, b.id))
-      world.removeEntity(b.id)
-      this.logger.debug(`Projectile hit enemy`, { id: a.id, damage })
+    if (bIsProjectile) {
+      this.processProjectileHit(b, a, world)
+    }
+  }
+
+  private processProjectileHit(
+    projectileEntity: Entity,
+    target: Entity,
+    world: World
+  ) {
+    const projectile = projectileEntity.getComponent<ProjectileComponent>(
+      COMPONENT_TYPES.projectile
+    )
+    if (!projectile) return
+
+    if (projectile.sourceId === target.id) {
+      return
+    }
+
+    if (target.hasComponent(COMPONENT_TYPES.health)) {
+      const damage = projectile.damage ?? 10
+      target.addComponent(
+        new DamageComponent({ amount: damage, sourceId: projectileEntity.id })
+      )
+      world.removeEntity(projectileEntity.id)
+      this.logger.debug(`Projectile hit entity`, { id: target.id, damage })
+      return
+    }
+
+    if (target.hasComponent(COMPONENT_TYPES.obstacle)) {
+      world.removeEntity(projectileEntity.id)
+      return
+    }
+
+    if (!target.hasComponent(COMPONENT_TYPES.projectile)) {
+      world.removeEntity(projectileEntity.id)
     }
   }
 }

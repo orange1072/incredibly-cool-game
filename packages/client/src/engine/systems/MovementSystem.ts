a23aca728133b2bc565441ff9f69f305b9dba078
Streltsov Ivan <principdereva@gmail.com>
2025-10-15 21:35:08 +0200

    feat: movement system

--- packages/client/src/engine/systems/MovementSystem.ts
+++ packages/client/src/engine/systems/MovementSystem.ts
@@ -1,13 +1,16 @@
-import { ISystem } from '../../types/engine.types'
+import { COMPONENT_TYPES, ISystem } from '../../types/engine.types'
 import { PositionComponent, VelocityComponent } from '../core/Components'
 import World from '../core/World'
 
 class MovementSystem implements ISystem {
   update(world: World, dt: number) {
-    const entities = world.query('position', 'velocity')
+    const entities = world.query(
+      COMPONENT_TYPES.position,
+      COMPONENT_TYPES.velocity
+    )
     for (const e of entities) {
-      const pos = e.getComponent<PositionComponent>('position')
-      const vel = e.getComponent<VelocityComponent>('velocity')
+      const pos = e.getComponent<PositionComponent>(COMPONENT_TYPES.position)
+      const vel = e.getComponent<VelocityComponent>(COMPONENT_TYPES.velocity)
       if (pos && vel) {
         pos.x += vel.dx * dt
         pos.y += vel.dy * dt

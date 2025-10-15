9e8e1fb19d537a2bfe8e836f01ff59015508de44
Streltsov Ivan <principdereva@gmail.com>
2025-10-15 22:00:41 +0200

    feat: added exp system

--- packages/client/src/engine/systems/ExpirienceSystem.ts
+++ packages/client/src/engine/systems/ExpirienceSystem.ts
@@ -1,4 +1,4 @@
-import { ISystem } from '../../types/engine.types'
+import { COMPONENT_TYPES, ISystem } from '../../types/engine.types'
 import { ExperienceComponent } from '../core/Components'
 import World from '../core/World'
 import EventBus from '../infrastructure/EventBus'
@@ -10,51 +10,114 @@ interface ExperienceSystemOptions {
 
 class ExperienceSystem implements ISystem {
   private eventBus: EventBus
+  private world: World | null = null
+  private pendingRewards: EnemyKilledPayload[] = []
   private logger = new Logger('ExperienceSystem', 'info')
+
+  private handleEnemyKilled = (rawData: unknown) => {
+    const data = rawData as Partial<EnemyKilledPayload> | undefined
+    if (!data?.killerId) return
+
+    const xpReward = Number(data.xpReward ?? 0)
+    if (!Number.isFinite(xpReward) || xpReward <= 0) {
+      this.logger.debug('Ignoring non-positive XP reward', data)
+      return
+    }
+
+    if (!this.world) {
+      this.pendingRewards.push({ killerId: data.killerId, xpReward })
+      return
+    }
+
+    this.grantExperience(data.killerId, xpReward)
+  }
 
   constructor({ eventBus }: ExperienceSystemOptions) {
     this.eventBus = eventBus
+    this.eventBus.on('enemyKilled', this.handleEnemyKilled)
+  }
 
-    this.eventBus.on('enemyKilled', (data) => {
-      this.onEnemyKilled(data as { killerId: string; xpReward: number })
-    })
+  initialize(world: World): void {
+    this.world = world
+    this.flushPendingRewards()
   }
 
   update(world: World, dt: number): void {
-    console.log(dt)
-    const players = world.query('experience')
+    this.world = world
+    this.flushPendingRewards()
+
+    const players = world.query(COMPONENT_TYPES.experience)
     for (const player of players) {
-      const exp = player.getComponent<ExperienceComponent>('experience')
+      const exp = player.getComponent<ExperienceComponent>(
+        COMPONENT_TYPES.experience
+      )
       if (!exp) continue
 
-      if (exp.xp >= exp.xpToNext) {
-        exp.xp -= exp.xpToNext
-        exp.level += 1
-        exp.xpToNext = Math.floor(exp.xpToNext * 1.25)
+      this.processLevelUps(player.id, exp)
+    }
+    console.log({ dt })
+  }
 
-        this.eventBus.emit('playerLevelUp', {
-          id: player.id,
-          newLevel: exp.level,
-        })
-        this.logger.info(`Level up! ${exp.level}`)
-      }
+  private grantExperience(killerId: string, xpReward: number) {
+    if (!this.world) return
+    const killer = this.world.getEntity(killerId)
+    const exp = killer?.getComponent<ExperienceComponent>(
+      COMPONENT_TYPES.experience
+    )
+    if (!exp) {
+      this.logger.debug('Entity without experience component received XP', {
+        killerId,
+      })
+      return
+    }
+
+    exp.xp += xpReward
+
+    this.logger.debug('Experience awarded', {
+      killerId,
+      xpReward,
+      total: exp.xp,
+    })
+  }
+
+  private flushPendingRewards() {
+    if (!this.world || this.pendingRewards.length === 0) return
+
+    const rewards = this.pendingRewards.splice(0)
+    for (const reward of rewards) {
+      this.grantExperience(reward.killerId, reward.xpReward)
     }
   }
 
-  private onEnemyKilled({
-    killerId,
-    xpReward,
-  }: {
-    killerId: string
-    xpReward: number
-  }) {
-    console.log({ killerId }, { xpReward })
-    // const world = (globalThis as any).engineWorld as World
-    // const killer = world.getEntity(killerId)
-    // const exp = killer?.getComponent<ExperienceComponent>('experience')
-    // if (!exp) return
-    // exp.xp += xpReward
+  private processLevelUps(entityId: string, exp: ExperienceComponent) {
+    let leveledUp = false
+
+    while (exp.xp >= exp.xpToNext && exp.xpToNext > 0) {
+      exp.xp -= exp.xpToNext
+      exp.level += 1
+      exp.xpToNext = Math.max(1, Math.floor(exp.xpToNext * 1.25))
+      leveledUp = true
+
+      this.eventBus.emit('playerLevelUp', {
+        id: entityId,
+        newLevel: exp.level,
+      })
+    }
+
+    if (leveledUp) {
+      this.logger.info('Player level up', {
+        id: entityId,
+        level: exp.level,
+        remainingXp: exp.xp,
+        nextThreshold: exp.xpToNext,
+      })
+    }
   }
+}
+
+interface EnemyKilledPayload {
+  killerId: string
+  xpReward: number
 }
 
 export default ExperienceSystem

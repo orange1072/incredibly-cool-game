b49eaf4ecb7103f8f796ce875dbe094feb7194c4
Streltsov Ivan <principdereva@gmail.com>
2025-10-15 21:07:52 +0200

    feat: react adapter

--- packages/client/src/engine/adapters/ReactAdapter.ts
+++ packages/client/src/engine/adapters/ReactAdapter.ts
@@ -1,7 +1,5 @@
 import { RefObject } from 'react'
 import GameEngine from '../core/GameEngine'
-import Renderer from '../systems/RenderSystem'
-
 interface ReactAdapterOptions {
   canvasRef: RefObject<HTMLCanvasElement>
   engine: GameEngine
@@ -10,7 +8,6 @@ interface ReactAdapterOptions {
 export class ReactAdapter {
   private engine: GameEngine
   private canvasRef: RefObject<HTMLCanvasElement>
-  private rafId: number | null = null
   private isRunning = false
 
   constructor({ canvasRef, engine }: ReactAdapterOptions) {
@@ -19,25 +16,17 @@ export class ReactAdapter {
   }
 
   start() {
+    if (this.isRunning) return
     const canvas = this.canvasRef.current
     if (!canvas) throw new Error('Canvas not ready')
 
-    const renderer = new Renderer(canvas)
-    this.engine = new GameEngine(renderer)
-
     this.engine.start()
     this.isRunning = true
-
-    const loop = () => {
-      if (!this.isRunning) return
-      this.rafId = requestAnimationFrame(loop)
-    }
-    this.rafId = requestAnimationFrame(loop)
   }
 
   stop() {
+    if (!this.isRunning) return
     this.isRunning = false
-    if (this.rafId) cancelAnimationFrame(this.rafId)
     this.engine.stop()
   }
 

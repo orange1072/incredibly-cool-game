34c464c1536fb099da3a77923c397255894f3e9d
Nazarian Art <ironarchier@gmail.com>
2025-10-16 09:02:15 +0300

    feat: 1162694 - review fixes

--- packages/client/src/pages/SignupPage/SignupPage.tsx
+++ packages/client/src/pages/SignupPage/SignupPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import {
   User,
   Mail,
@@ -14,10 +14,20 @@ import styles from './Registration.module.scss'
 import { useNavigate } from 'react-router-dom'
 import { ParticleBackground } from '@/components/ParticleBackground'
 
+type FormData = {
+  firstName: string
+  lastName: string
+  email: string
+  phone: string
+  username: string
+  password: string
+  confirmPassword: string
+}
+
 export const SignupPage = () => {
   const navigate = useNavigate()
   const [step, setStep] = useState(1)
-  const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState<FormData>({
     firstName: '',
     lastName: '',
     email: '',
@@ -50,15 +60,24 @@ export const SignupPage = () => {
     return Object.keys(newErrors).length === 0
   }
 
-  const handleNext = () => {
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
     if (validateStep(step)) {
       if (step < 3) {
         setStep(step + 1)
       } else {
-        navigate('profile')
+        navigate('/profile')
       }
     }
   }
+
+  const handleChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      const { name, value } = e.target
+      setFormData((prev) => ({ ...prev, [name]: value }))
+    },
+    []
+  )
 
   const steps = ['Personal Data', 'Contacts', 'Access ID']
 
@@ -66,12 +85,12 @@ export const SignupPage = () => {
     names.filter(Boolean).join(' ')
 
   return (
-    <div className={styles['registration-page']}>
+    <main className={styles['registration-page']}>
       <ParticleBackground particleCount={20} />
 
       <div className={styles['registration-page__content']}>
         <div className={styles.container}>
-          <div className={styles.header}>
+          <header className={styles.header}>
             <div className={styles['logo-wrap']}>
               <div className={`${styles.glow}`} />
               <Shield className={cx(styles.icon, styles.shield)} />
@@ -80,9 +99,9 @@ export const SignupPage = () => {
             <h1 className={styles['stalker-text']}>STALKER REGISTRATION</h1>
 
             <p className={styles.subtitle}>Join the Z.O.N.E. Expedition</p>
-          </div>
+          </header>
 
-          <div className={styles.progress}>
+          <nav className={styles.progress}>
             {steps.map((stepName, index) => (
               <div key={index} className={styles['progress__segment']}>
                 <div className={styles['progress__inner']}>
@@ -108,9 +127,9 @@ export const SignupPage = () => {
                 <div className={styles['progress__label']}>{stepName}</div>
               </div>
             ))}
-          </div>
+          </nav>
 
-          <div className={styles['metal-panel']}>
+          <section className={styles['metal-panel']}>
             <div className={styles['top-line']} />
 
             <div className={styles['form-inner']}>
@@ -120,130 +139,128 @@ export const SignupPage = () => {
                 <span className={styles['status-text']}>Step {step}/3</span>
               </div>
 
-              {step === 1 && (
-                <div className={styles['grid-2']}>
-                  <Input
-                    label="First Name"
-                    placeholder="Alex"
-                    icon={<User className={styles['input-icon']} />}
-                    value={formData.firstName}
-                    onChange={(e) =>
-                      setFormData({ ...formData, firstName: e.target.value })
-                    }
-                    error={errors.firstName}
-                  />
+              <form onSubmit={handleSubmit}>
+                {step === 1 && (
+                  <fieldset className={styles['grid-2']}>
+                    <legend style={{ display: 'none' }}>Personal data</legend>
 
-                  <Input
-                    label="Last Name"
-                    placeholder="Degtyarev"
-                    icon={<User className={styles['input-icon']} />}
-                    value={formData.lastName}
-                    onChange={(e) =>
-                      setFormData({ ...formData, lastName: e.target.value })
-                    }
-                    error={errors.lastName}
-                  />
-                </div>
-              )}
+                    <Input
+                      name={'firstName'}
+                      label="First Name"
+                      placeholder="Alex"
+                      icon={<User className={styles['input-icon']} />}
+                      value={formData.firstName}
+                      onChange={handleChange}
+                      error={errors.firstName}
+                    />
 
-              {step === 2 && (
-                <div className={styles['grid-2-2']}>
-                  <Input
-                    label="Email"
-                    type="email"
-                    placeholder="stalker@zone.net"
-                    icon={<Mail className={styles['input-icon']} />}
-                    value={formData.email}
-                    onChange={(e) =>
-                      setFormData({ ...formData, email: e.target.value })
-                    }
-                    error={errors.email}
-                  />
-
-                  <Input
-                    label="Phone"
-                    type="tel"
-                    placeholder="+1 (555) 000-0000"
-                    icon={<Phone className={styles['input-icon']} />}
-                    value={formData.phone}
-                    onChange={(e) =>
-                      setFormData({ ...formData, phone: e.target.value })
-                    }
-                    error={errors.phone}
-                  />
-                </div>
-              )}
-
-              {step === 3 && (
-                <div className={styles['grid-2-2']}>
-                  <Input
-                    label="Call Sign"
-                    placeholder="marked_one"
-                    icon={<User className={styles['input-icon']} />}
-                    value={formData.username}
-                    onChange={(e) =>
-                      setFormData({ ...formData, username: e.target.value })
-                    }
-                    error={errors.username}
-                  />
-
-                  <Input
-                    label="Access Code"
-                    type="password"
-                    placeholder="••••••••"
-                    icon={<Lock className={styles['input-icon']} />}
-                    value={formData.password}
-                    onChange={(e) =>
-                      setFormData({ ...formData, password: e.target.value })
-                    }
-                    error={errors.password}
-                  />
-
-                  <Input
-                    label="Confirm Code"
-                    type="password"
-                    placeholder="••••••••"
-                    icon={
-                      formData.password === formData.confirmPassword &&
-                      formData.confirmPassword ? (
-                        <CheckCircle className={styles['input-icon']} />
-                      ) : (
-                        <Lock className={styles['input-icon']} />
-                      )
-                    }
-                    value={formData.confirmPassword}
-                    onChange={(e) =>
-                      setFormData({
-                        ...formData,
-                        confirmPassword: e.target.value,
-                      })
-                    }
-                    error={errors.confirmPassword}
-                  />
-                </div>
-              )}
-
-              <div className={styles.actions}>
-                {step > 1 && (
-                  <PixelButton
-                    variant="secondary"
-                    size="lg"
-                    className={cx(styles.btn, styles['flex-1'])}
-                    onClick={() => setStep(step - 1)}
-                  >
-                    ← Back
-                  </PixelButton>
+                    <Input
+                      name={'lastName'}
+                      label="Last Name"
+                      placeholder="Degtyarev"
+                      icon={<User className={styles['input-icon']} />}
+                      value={formData.lastName}
+                      onChange={handleChange}
+                      error={errors.lastName}
+                    />
+                  </fieldset>
                 )}
 
-                <PixelButton
-                  variant="primary"
-                  size="lg"
-                  className={cx(styles.btn, styles['flex-1'])}
-                  onClick={handleNext}
-                >
-                  {step === 3 ? 'Complete Registration' : 'Next →'}
-                </PixelButton>
-              </div>
+                {step === 2 && (
+                  <fieldset className={styles['grid-2-2']}>
+                    <legend style={{ display: 'none' }}>Contacts</legend>
+
+                    <Input
+                      name={'email'}
+                      label="Email"
+                      type="email"
+                      placeholder="stalker@zone.net"
+                      icon={<Mail className={styles['input-icon']} />}
+                      value={formData.email}
+                      onChange={handleChange}
+                      error={errors.email}
+                    />
+
+                    <Input
+                      name={'phone'}
+                      label="Phone"
+                      type="tel"
+                      placeholder="+1 (555) 000-0000"
+                      icon={<Phone className={styles['input-icon']} />}
+                      value={formData.phone}
+                      onChange={handleChange}
+                      error={errors.phone}
+                    />
+                  </fieldset>
+                )}
+
+                {step === 3 && (
+                  <fieldset className={styles['grid-2-2']}>
+                    <legend style={{ display: 'none' }}>Access</legend>
+
+                    <Input
+                      name={'username'}
+                      label="Call Sign"
+                      placeholder="marked_one"
+                      icon={<User className={styles['input-icon']} />}
+                      value={formData.username}
+                      onChange={handleChange}
+                      error={errors.username}
+                    />
+
+                    <Input
+                      name={'password'}
+                      label="Access Code"
+                      type="password"
+                      placeholder="••••••••"
+                      icon={<Lock className={styles['input-icon']} />}
+                      value={formData.password}
+                      onChange={handleChange}
+                      error={errors.password}
+                    />
+
+                    <Input
+                      name={'confirmPassword'}
+                      label="Confirm Code"
+                      type="password"
+                      placeholder="••••••••"
+                      icon={
+                        formData.password === formData.confirmPassword &&
+                        formData.confirmPassword ? (
+                          <CheckCircle className={styles['input-icon']} />
+                        ) : (
+                          <Lock className={styles['input-icon']} />
+                        )
+                      }
+                      value={formData.confirmPassword}
+                      onChange={handleChange}
+                      error={errors.confirmPassword}
+                    />
+                  </fieldset>
+                )}
+
+                <div className={styles.actions}>
+                  {step > 1 && (
+                    <PixelButton
+                      variant="secondary"
+                      size="lg"
+                      className={cx(styles.btn, styles['flex-1'])}
+                      onClick={() => setStep(step - 1)}
+                    >
+                      ← Back
+                    </PixelButton>
+                  )}
+
+                  <PixelButton
+                    type="submit"
+                    variant="primary"
+                    size="lg"
+                    className={cx(styles.btn, styles['flex-1'])}
+                  >
+                    {step === 3 ? 'Complete Registration' : 'Next →'}
+                  </PixelButton>
+                </div>
+              </form>
 
               <div className={styles['footer-link']}>
                 <button
@@ -256,9 +273,9 @@ export const SignupPage = () => {
             </div>
 
             <div className={styles['bottom-line']} />
-          </div>
+          </section>
         </div>
       </div>
-    </div>
+    </main>
   )
 }

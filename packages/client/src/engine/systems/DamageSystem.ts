7c394cf93faadcb66d08a3608b41cb479799a605
Streltsov Ivan <principdereva@gmail.com>
2025-10-16 12:55:15 +0200

    feat: enemy can hit player, no death of player implemented

--- packages/client/src/engine/systems/DamageSystem.ts
+++ packages/client/src/engine/systems/DamageSystem.ts
@@ -1,5 +1,9 @@
 import { COMPONENT_TYPES, ISystem } from '../../types/engine.types'
-import { DamageComponent, HealthComponent } from '../core/Components'
+import {
+  DamageComponent,
+  HealthComponent,
+  EnemyComponent,
+} from '../core/Components'
 import World from '../core/World'
 import EventBus from '../infrastructure/EventBus'
 
@@ -18,14 +22,24 @@ class DamageSystem implements ISystem {
       health.hp -= damage.amount
 
       if (health.hp <= 0) {
-        world.removeEntity(e.id)
+        const enemy = e.getComponent<EnemyComponent>(COMPONENT_TYPES.enemy)
+        const killerId = damage.sourceId
+        const xpReward = enemy?.xpReward ?? 0
 
         world.removeEntity(e.id)
-        bus.emit('enemyKilled', {
-          killerId: damage.sourceId,
-          xpReward: 20,
-        })
+
+        //to-do: смерть игрока
+
+        //здесь ниже реализована только смерть противника и события по этому поводу.
+        if (killerId && xpReward > 0) {
+          bus.emit('enemyKilled', {
+            killerId,
+            xpReward,
+          })
+        }
       }
+
+      e.removeComponent(COMPONENT_TYPES.damage)
     }
   }
 }

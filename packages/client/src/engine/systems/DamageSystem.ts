b1f77ba384938204ef53cacfd7f02d2695c7105a
Streltsov Ivan <principdereva@gmail.com>
2025-10-27 17:27:26 +0100

    feat: get killed fuction and get loot function

--- packages/client/src/engine/systems/DamageSystem.ts
+++ packages/client/src/engine/systems/DamageSystem.ts
@@ -6,17 +6,21 @@ import {
 } from '../../types/engine.types';
 import {
   DamageComponent,
-  HealthComponent,
   EnemyComponent,
+  HealthComponent,
+  PositionComponent,
 } from '../components';
 import World from '../core/World';
+import { createLoot } from '../enteties/createLoot';
 import EventBus from '../infrastructure/EventBus';
+import Logger from '../infrastructure/Logger';
 import { ZERO_HP, ZERO_XP_REWARD } from './consts/damage';
 
 const bus = EventBus.instance;
 
 class DamageSystem implements ISystem<SystemType> {
   type: SystemType = SYSTEM_TYPES.damage as SystemType;
+  private logger = new Logger('XP count');
   update(world: World) {
     const entities = world.query(
       COMPONENT_TYPES.health,
@@ -33,24 +37,50 @@ class DamageSystem implements ISystem<SystemType> {
 
       if (health.hp <= ZERO_HP) {
         const enemy = e.getComponent<EnemyComponent>(COMPONENT_TYPES.enemy);
+        const isEnemy = e.hasComponent(COMPONENT_TYPES.enemy);
+        const enemyPosition = e.getComponent<PositionComponent>(
+          COMPONENT_TYPES.position
+        );
+
+        const isPlayer = e.hasComponent(COMPONENT_TYPES.playerControl);
         const killerId = damage.sourceId;
         const xpReward = enemy?.xpReward ?? ZERO_XP_REWARD;
 
         world.removeEntity(e.id);
 
-        //to-do: смерть игрока
+        if (isPlayer) {
+          this.getKilled();
+        }
 
-        //здесь ниже реализована только смерть противника и события по этому поводу.
-        if (killerId && xpReward > 0) {
-          bus.emit('enemyKilled', {
-            killerId,
-            xpReward,
-          });
+        if (isEnemy && killerId && xpReward > 0 && enemyPosition) {
+          this.getLoot(killerId, enemyPosition, xpReward, world);
         }
       }
 
       e.removeComponent(COMPONENT_TYPES.damage);
     }
+  }
+
+  getKilled() {
+    bus.emit('playerKilled');
+  }
+
+  getLoot(
+    killerId: string,
+    enemyPosition: PositionComponent,
+    xpReward: number,
+    world: World
+  ) {
+    bus.emit('enemyKilled', {
+      killerId,
+    });
+
+    this.logger.info('xp loot', {
+      enemy: enemyPosition.type,
+      pos: enemyPosition,
+    });
+    const loot = createLoot(enemyPosition, 'xp', xpReward);
+    world.addEntity(loot);
   }
 }
 

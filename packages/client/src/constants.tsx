3ee728e6a44c857809c9175c132bc14822c37062
Julia Shlykova <julia.shlykova17@gmail.com>
2025-12-22 10:14:14 +0700

    move withFullScrenn.tsx to hocs

--- packages/client/src/constants.tsx
+++ packages/client/src/constants.tsx
@@ -25,3 +25,18 @@ export const PROFILE_STATS = [
     Icon: <GuardSVG />,
   },
 ];
+
+export const ERROR_MESSAGES = {
+  AVATAR: {
+    INVALID_FILE_TYPE: 'Please upload an image file',
+    FILE_TOO_LARGE: 'File size should not exceed 5MB',
+    UPLOAD_FAILED: 'Failed to upload avatar. Please try again.',
+  },
+  PASSWORD: {
+    ALL_FIELDS_REQUIRED: 'All fields are required',
+    PASSWORD_TOO_SHORT: 'New password must be at least 6 characters',
+    PASSWORDS_DO_NOT_MATCH: 'New passwords do not match',
+    CHANGE_FAILED:
+      'Failed to change password. Please check your current password.',
+  },
+};

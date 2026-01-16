253f17e72c71901ed41f0fdf81a1055251f60410
Sergey Gatsenko <ya.stealth1072@yandex.ru>
2026-01-16 21:41:39 +0300

    feat(supabase) Обновил лидерборд и подключил базу

--- packages/client/src/pages/Leaderboard/LeaderBoard.tsx
+++ packages/client/src/pages/Leaderboard/LeaderBoard.tsx
@@ -1,6 +1,4 @@
-import { Search } from './components/Search';
 import { Table } from './components/Table';
-import { Pagination } from './components/Pagination';
 import { Header } from './components/Header';
 import styles from './Leaderboard.module.scss';
 
@@ -8,9 +6,7 @@ export const LeaderboardPage = () => {
   return (
     <div className={styles.main}>
       <Header />
-      <Search />
       <Table />
-      <Pagination />
     </div>
   );
 };
